7bd8689b3624a441ee1a7e50a010b127e696cafc
huny <hy546880109@qq.com>
2022-04-21 23:39:51 +0800

    优化电信回调数据接口

--- test_case/web_test_case/Callback_Controller/test_dx_api.py
+++ test_case/web_test_case/Callback_Controller/test_dx_api.py
@@ -2,8 +2,8 @@
 # _*_ codeing: utf-8 _*_
 import unittest,os,sys,json
 
-path = os.path.join(os.path.dirname(os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__)))))
+path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))))))
 sys.path.append(path)
 from config.config_test import Conf
 from common.http_requests import HttpRequests
@@ -27,15 +27,14 @@ class Test_Detele_Device(unittest.TestCase):
         cls.mysql.close()
 
     def test_delete_device_success(self):
-        """���Żص��ɹ�������/cwting/callback"""
-        payload = {"id": Test_Detele_Device.device_id}
-        response = Test_Detele_Device.http.get(
-            '/cwting/callback', params=payload)
-        self.assertEqual(200, response.status_code, '���ط�200')
-        self.assertEqual(str(0), str(response.json()['code']), 'ɾ���Ѿ�ʧ��')
-        self.in_mysql = self.mysql.select_sql(
-            'select is_delete from t_cellar_well where id={}'.format(Test_Detele_Device.device_id))  # �ٴβ�ѯɾ����ʶλ
-        self.assertEqual(1, self.in_mysql, '���ݿ�δɾ����ɾ���Ѿ�����ִ��ʧ��')
+        """电信回调数据/cwting/callback"""
+        payload = {"upPacketSN":-1,"upDataSN":-1,"topic":"v1/up/ad","timestamp":1650554828842,"tenantId":"2000033629","serviceId":"","protocol":"lwm2m","productId":"15114816","payload":{"APPdata":"AQFWMS4wAAAAACMAAAAH/wAlODYyNTkyMDUxNjUxNDcyAAEAMjUAODAAAAAAAAABAQAAAAEAAASD"},"messageType":"dataReport","deviceType":"","deviceId":"40edca19b0124480864366de4621de99","assocAssetId":"","IMSI":"undefined","IMEI":"862592051651472"}
+        payload = json.dumps(payload)
+        response = Test_Detele_Device.http.post(
+            '/cwting/callback', data=payload)
+        self.assertEqual(200, response.status_code, '返回非200')
+        # self.assertEqual(str(0), str(response.json()['code']), '电信回调数据失败')
+
 
 
 if __name__ == '__main__':
